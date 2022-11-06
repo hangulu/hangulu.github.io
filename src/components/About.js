18e5d6e19f2ff4a1fb277ea4471d6082cb456766
Hakeem Angulu <hakeem.angulu@gmail.com>
2022-11-06 16:54:42 -0500

    Update About and Project pages.

--- src/components/About.js
+++ src/components/About.js
@@ -15,32 +15,31 @@ export default function About({ mobile }) {
           <p>
             My name is <strong>Hakeem Olakunle Isa Angulu</strong>. I was born
             and raised in Kingston, Jamaica, and I moved to the United States in
-            2014 to finish my education.
+            2014 to finish my education. I am currently based in Brooklyn, NY.
           </p>
           <p>
             In May 2020, I graduated from Harvard College with a Bachelor of
             Arts degree in Computer Science and Statistics, and a secondary
-            field of African American Studies.
-          </p>
-          <p>
-            I am currently based in Brooklyn, NY. I am a Software Engineer III
-            at Google, on their Counter-Abuse Technology team. I am also the
-            Chief Technology Officer of{" "}
+            field of African American Studies. I am now a Software Engineer III
+            at Google, on their{" "}
             <a
-              href="https://myoaksystem.com/"
+              href="https://io.google/2022/program/d7b6d22e-59a4-4e6b-a49d-3116ee57dcc3/"
               target="_blank"
               rel="noopener noreferrer">
-              oak
+              Counter-Abuse Technology team{" "}
             </a>
-            , a startup committed to uplifting Black-owned hair businesses and
-            making natural hair accessible using science and technology.
+            , within the Privacy, Safety and Security organization.
           </p>
           <p>
-            I have two primary passions: art and code. This website aims to be a
-            showcase of those two things, both separately and as one. I document
-            here my own artwork (mostly photography) and my own coding projects,
-            many of which have the explicit aim to amplify voices, facilitate
-            justice and push limits.
+            I'm an optimistic builder. I translate my ideas — and those from
+            others that I believe in and collaborate with — into virtual and
+            physical objects people can use and relate to. My usual tools for
+            this process are code and art (photography, cinematography, and
+            curation). To serve as a proper introduction, this website is also a
+            showcase of what I've built with those tools. I document here my own
+            artwork (mostly photography) and my own non-Google software
+            projects, many of which have the explicit aim to amplify voices,
+            facilitate justice and push limits.
           </p>
           <p>
             If you'd like to get in touch with me, please use{" "}
